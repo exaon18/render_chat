0686987066329dedf93a0097e70ee17e27c24b42
exaon18 <69327021+exaon18@users.noreply.github.com>
2024-12-14 23:03:47 -0800

    Update settings.py

--- realchat/settings.py
+++ realchat/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
-
+ CORS_ALLOWED_ORIGINS =[
+     'https://render-chat-3.onrender.com'
+ ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
